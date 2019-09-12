346cdcc7665a5f933a1c2aafe5df68df63723f9b
EconClass <bizmail327@gmail.com>
2019-09-12 14:25:47 -0700

    [add] lifted state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from "react";
 import "./App.css";
 
-import SVG from "./components/SVG";
+// import SVG from "./components/SVG";
 
 import Counter from "./components/Counter";
+import Title from "./components/Title";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Counts in 1s */}
-        <Counter />
-        {/* Counts in 5s */}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+  increment(modifier) {
+    this.setState({ count: this.state.count + modifier })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Title text={this.state.count} />
+          {/* Counts in 1s */}
+          <Counter modifier={1} onClick={() => { this.increment(1) }} />
 
-        {/* Counts in 3s */}
+          {/* Counts in 5s */}
+          <Counter modifier={5} onClick={() => { this.increment(5) }} />
 
-        <SVG />
-      </header>
-    </div>
-  );
+          {/* Counts in 3s */}
+          <Counter modifier={3} onClick={() => { this.increment(3) }} />
+          {/* <SVG /> */}
+        </header>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* Counts in 1s */}
+//         <Counter modifier={1} />
+
+//         {/* Counts in 5s */}
+//         <Counter modifier={5} />
+
+//         {/* Counts in 3s */}
+//         <Counter modifier={3} />
+//         {/* <SVG /> */}
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
-import Title from "./Title";
+// import Title from "./Title";
 import "./Counter.css";
 
 // Define a new Component with a class
 // It must extend Component (which we imported above)
 class Counter extends Component {
-  constructor() {
-    super(); // You must call super() in the constructor!
+  // constructor(props) {
+  //   super(props); // You must call super() in the constructor!
 
-    // State is an object with properties
-    // In this case there is one property: count
-    this.state = {
-      count: 0
-    };
-  }
+  //   // State is an object with properties
+  //   // In this case there is one property: count
+  //   // this.state = {
+  //   //   modifier: props.modifier,
+  //   // };
+  // }
 
   render() {
     return (
       <div className="Counter">
         {/* This element displays the value of this.state.count */}
-        <Title text={this.state.count} />
+        {/* <Title text={this.state.count} /> */}
 
         {/* This button will change this.state.count each time it is clicked
         by adding one. You must change state by calling this.setState() */}
         <button
           onClick={() => {
-            this.setState({ count: this.state.count + 1 });
+            this.props.onClick()
           }}
           className="Counter--button"
         >
-          Count
+          Count by {this.props.modifier}
         </button>
       </div>
     );
